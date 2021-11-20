873daf046039f36b675b77d34ff6a9eec6f53095
Peter Plavskikh <qksyo@yandex.ru>
2021-11-21 00:40:29 +0700

    hotfixes after review

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -2,21 +2,19 @@ let popup = document.querySelector('.popup');
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('#popup__name');
-let statusInput = document.querySelector('#popup__status');
+let nameInput = document.querySelector('[name="user_name"]');
+let statusInput = document.querySelector('[name="user_status"]');
 let nameStart = document.querySelector('.profile__name');
 let statusStart = document.querySelector('.profile__status');
 
 function showpop() {
-  openPopupButton.addEventListener('click',
-  popup.classList.add('popup_opened'))
+  popup.classList.add('popup_opened');
   nameInput.value = nameStart.textContent;
   statusInput.value = statusStart.textContent;
 }
 
 function hidepop() {
-  closePopupButton.addEventListener('click',
-  popup.classList.remove('popup_opened'))
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
@@ -25,5 +23,6 @@ function formSubmitHandler (evt) {
   statusStart.textContent = statusInput.value;
   hidepop();
 }
-
+openPopupButton.addEventListener('click', showpop);
+closePopupButton.addEventListener('click', hidepop);
 formElement.addEventListener('submit', formSubmitHandler);
