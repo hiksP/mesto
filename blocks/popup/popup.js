cd55fccd66cee225a2967f2d10195694b4ddd542
Peter Plavskikh <qksyo@yandex.ru>
2021-12-02 21:44:20 +0700

    feat: delete button is done but not working

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -24,27 +24,6 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-const templateBox = document.querySelector('.template');
-const ListContainer = document.querySelector('.elements__list');
-
-function render() {
-  const html = initialCards
-    .map((item) => {
-      return getItem(item);
-    });
-    ListContainer.append(...html);
-}
-
-function getItem(item) {
-  const newItem = templateBox.content.cloneNode(true);
-  const headerEl = newItem.querySelector('.elements__title');
-  const imageEL = newItem.querySelector('.elements__image');
-  headerEl.textContent = item.name;
-  imageEL.src = item.link;
-  return newItem;
-}
-
-render()
 
 let popupEdit = document.querySelector('.popup_edit');
 let openPopupButton = document.querySelector('.profile__edit-button');
@@ -56,6 +35,26 @@ let nameStart = document.querySelector('.profile__name');
 let statusStart = document.querySelector('.profile__status');
 let placeInput = document.querySelector('[name="place_name"]');
 let linkInput = document.querySelector('[name="place_link"]');
+const addButton = document.querySelector('.profile__add-button');
+const templateBox = document.querySelector('.template');
+const listContainer = document.querySelector('.elements__list');
+
+function render() {
+  const html = initialCards
+    .map((item) => {
+      return getItem(item);
+    });
+    listContainer.append(...html);
+}
+
+function getItem(item) {
+  const newItem = templateBox.content.cloneNode(true);
+  const headerEl = newItem.querySelector('.elements__title');
+  const imageEL = newItem.querySelector('.elements__image');
+  headerEl.textContent = item.name;
+  imageEL.src = item.link;
+  return newItem;
+}
 
 function showpop() {
   popupEdit.classList.add('popup_opened');
@@ -73,6 +72,17 @@ function formSubmitHandler (evt) {
   statusStart.textContent = statusInput.value;
   hidepop();
 }
+
+function handleAdd()  {
+  const placeText = placeInput.value;
+  const placeLink = linkInput.value;
+  const cardItem = getItem({title: placeText, link: placeLink});
+  listContainer.prepend(cardItem);
+  placeInput.value = '';
+}
+
 openPopupButton.addEventListener('click', showpop);
 closePopupButton.addEventListener('click', hidepop);
 formElement.addEventListener('submit', formSubmitHandler);
+
+render()
