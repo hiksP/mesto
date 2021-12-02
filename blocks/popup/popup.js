1667e4f29fd59069e45a3d19c3642ef96adf4601
Peter Plavskikh <qksyo@yandex.ru>
2021-12-02 19:20:13 +0700

    feat: cards are uploaded from js

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,3 +1,50 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+const templateBox = document.querySelector('.template');
+const ListContainer = document.querySelector('.elements__list');
+
+function render() {
+  const html = initialCards
+    .map((item) => {
+      return getItem(item);
+    });
+    ListContainer.append(...html);
+}
+function getItem(item) {
+  const newItem = templateBox.content.cloneNode(true);
+  const headerEl = newItem.querySelector('.elements__title');
+  const imageEL = newItem.querySelector('.elements__image');
+  headerEl.textContent = item.name;
+  imageEL.src = item.link;
+  return newItem;
+}
+
+render()
+
 let popup = document.querySelector('.popup');
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close');
@@ -26,3 +73,4 @@ function formSubmitHandler (evt) {
 openPopupButton.addEventListener('click', showpop);
 closePopupButton.addEventListener('click', hidepop);
 formElement.addEventListener('submit', formSubmitHandler);
+
