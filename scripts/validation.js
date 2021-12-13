4f6acf0df9db86d61de97195ec909849e6102824
Peter Plavskikh <qksyo@yandex.ru>
2021-12-14 02:33:32 +0700

    feat: validation is working(almost)

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,17 +1,31 @@
-const checkIfInputValid = (form, input) => {
+const showError = (form, input, errorMessageText, errorClass, inputErrorClass) => {
+  const errorMessage = form.querySelector(`#${input.name}-error`)
+  errorMessage.textContent = errorMessageText
+  errorMessage.classList.add(errorClass);
+  input.classList.add(inputErrorClass);
+}
+
+const hideError = (form, input, errorClass, inputErrorClass) => {
+  const errorMessage = form.querySelector(`#${input.name}-error`)
+  errorMessage.textContent = ' '
+  errorMessage.classList.remove(errorClass);
+  input.classList.remove(inputErrorClass);
+}
+
+const checkIfInputValid = (form, input, {inputErrorClass, errorClass }) => {
   if (!input.validity.valid) {
-    console.log('nicht valid')
+    showError(form, input, input.validationMessage, inputErrorClass, errorClass)
   } else {
-    console.log('valid')
+    hideError(form, input, inputErrorClass, errorClass)
   }
 }
 
-const setInputListeners = (form, {inputSelector} ) => {
+const setInputListeners = (form, {inputSelector, ...rest} ) => {
   const inputs = form.querySelectorAll(inputSelector);
 
   inputs.forEach((input) => {
     input.addEventListener('input', () => {
-      checkIfInputValid(form, input);
+      checkIfInputValid(form, input, rest);
     });
   });
   
@@ -34,6 +48,6 @@ enableValidation({
   inputSelector: '.popup__data-box',
   submitButtonSelector: '.popup__submit',
   inactiveButtonClass: 'popup__button_inactive',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
+  inputErrorClass: 'popup__error_visible',
+  errorClass: 'popup__data-box_error'
 });
